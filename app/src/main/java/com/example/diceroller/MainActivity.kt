776c9acf8ec5e1bd11976ae37d1992da40bf90c4
TAOFEEK AKINTUNDE <58163594+taotechs@users.noreply.github.com>
2022-10-29 23:32:39 +0100

    Created A Dice Roller app

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,16 +37,20 @@ fun DiceRollerApp() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        DeviceWithButtonAndImage()
+        DiceWithButtonAndImage()
     }
 }
 
 @Composable
-fun DeviceWithButtonAndImage(
-    modifier: Modifier = Modifier
-) {
-    var result by remember { mutableStateOf(1) }
-    val imageResource = when (result) {
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    var textMe by remember { mutableStateOf(1)}
+    val textRes = when(textMe){
+        1 -> "Hello"
+        2 -> "What's up"
+        else -> "Your are welcome"
+    }
+    var result by remember { mutableStateOf( 1) }
+    val imageResource = when(result) {
         1 -> R.drawable.dice_1
         2 -> R.drawable.dice_2
         3 -> R.drawable.dice_3
@@ -53,23 +58,20 @@ fun DeviceWithButtonAndImage(
         5 -> R.drawable.dice_5
         else -> R.drawable.dice_6
     }
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Image(
-            painter = painterResource(id = imageResource),
-            contentDescription = result.toString(),
-                  //  modifier = modifier.background(color = Color.Yellow)
-        )
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text= textRes, fontSize = 50.sp)
+        Spacer(modifier = Modifier.height(16.dp))
+        Image(painter = painterResource(imageResource), contentDescription = result.toString())
         Spacer(modifier = Modifier.height(16.dp))
         Button(onClick = { result = (1..6).random() }) {
-            Text(stringResource(R.string.roll))
-            //movement
-
+            Text(text = stringResource(R.string.roll), fontSize = 24.sp)
         }
-    }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { textMe = (1..3).random() }){
+            Text(text = "Welcome Message")
+        }
 
+    }
 }
 
 @Preview(showBackground = true)
